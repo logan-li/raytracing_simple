1e0bba2122d83cb84d3df356db68f3ea61c8b17b
hao li <lihao1q87@qq.com>
2018-06-09 23:08:00 +0800

    generate ppm file

--- make_image.cpp
+++ make_image.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    int nx = 200;
+    int ny = 100;
+
+    ofstream file;
+    file.open("./assets/pic.ppm", ios::trunc);
+    file << "P3\n" << nx << " " << ny << "\n255\n";
+    
+    for (int j = ny - 1; j >= 0; j--)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            float r = (float)i / (float)nx;
+            float g = (float)j / (float)ny;
+            float b = 1;
+            int ir = int(255.99f * r);
+            int ig = int(255.99f * g);
+            int ib = int(255.99f * b);
+            file << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    file.close();
     return 0;
 }
